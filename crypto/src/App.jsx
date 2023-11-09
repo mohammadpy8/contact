47c3844753574eab06f5551cb1cf8c19214ce865
mohammadpy8 <mohammadpy8@gmail.com>
2023-11-09 14:57:12 +0330

    a

---
+++ crypto/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HomePage from "./components/template/HomePage";
+
+function App() {
+  return <HomePage />;
+}
+
+export default App;
---
+++ crypto/src/components/modules/TableCoin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TableCoin = ({ coins }) => {
+    console.log(coins);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TableCoin;
